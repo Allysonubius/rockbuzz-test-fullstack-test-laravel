c57882905c12300d393f882697fcf58608d50581
Allyson de Oliveira Brandão <allysomoliveira7@gmail.com>
2020-07-24 18:10:21 -0300

    Realizado alteração do codigo Post.

--- app/Post.php
+++ app/Post.php
@@ -8,8 +8,16 @@ class Post extends Model
 {
     protected $guarded = [];
 
-    protected $fillable = ['title', 'body', 'image', 'published', 'author', 'created_at', 'slug'];
+    public function has_tag($tag_id)
+    {
+        $rows = \DB::table('tag_post')->where('tag_id', '=', $tag_id)->where('post_id', '=', $this->id)->get();
 
+        if (count($rows) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
 
